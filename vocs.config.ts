3e47dc6a519bc644a720de9a48f12dc0ddbac5d0
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2024-05-15 23:34:05 +0530

    fix: create a sample mdx component for contract

--- vocs.config.ts
+++ vocs.config.ts
@@ -2,41 +2,22 @@ import { defineConfig } from "vocs";
 
 export default defineConfig({
   title: "Starknet by Example",
-  // topNav: [
-  //   { text: "Guide & API", link: "/docs/getting-started", match: "/docs" },
-  //   { text: "Blog", link: "/blog" },
-  //   {
-  //     text: "version",
-  //     items: [
-  //       {
-  //         text: "Changelog",
-  //         link: "https://github.com/wevm/vocs/blob/main/src/CHANGELOG.md",
-  //       },
-  //       {
-  //         text: "Contributing",
-  //         link: "https://github.com/wevm/vocs/blob/main/.github/CONTRIBUTING.md",
-  //       },
-  //     ],
-  //   },
-  // ],
   sidebar: [
     {
       text: "Getting Started",
-      collapsed: false,
       items: [
-        // {
-        //   text: "Config",
-        //   link: "/docs/api/config",
-        // },
-
         {
           text: "1. Basics of a Starknet contract",
           link: "/ch00/basics/introduction",
-          collapsed: true,
+          collapsed: false,
           items: [
             {
               text: "1.1. Storage",
               link: "/ch00/basics/storage",
+            },
+            {
+              text: "1.2. Constructor",
+              link: "/ch00/basics/constructor",
             },
           ],
         },
